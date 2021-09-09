7cabb75a0c8431f2bc392b7370313c9339c08a1e
Vincent Theonica <vincent085213@gmail.com>
2021-09-09 19:36:33 +0700

    edit keterangan

--- tambah-proses.php
+++ tambah-proses.php
@@ -8,12 +8,12 @@ if(isset($_POST['tambah'])){
 	include('koneksi.php');
 	
 	//jika tombol tambah benar di klik maka lanjut prosesnya
-	$nik				= $_POST['nik'];	//membuat variabel $nis dan datanya dari inputan NIS
-	$nama				= $_POST['nama'];	//membuat variabel $nama dan datanya dari inputan Nama Lengkap
-	$tanggal_lahir		= $_POST['tanggal_lahir'];	//membuat variabel $kelas dan datanya dari inputan dropdown Kelas
-	$no_hp				= $_POST['no_hp'];	//membuat variabel $jurusan dan datanya dari inputan dropdown Jurusan
-	$jam				= $_POST['jam'];
-	$menit				= $_POST['menit'];
+	$nik			= $_POST['nik'];		//membuat variabel $nik dan datanya dari inputan NIK
+	$nama			= $_POST['nama'];		//membuat variabel $nama dan datanya dari inputan Nama Lengkap
+	$tanggal_lahir		= $_POST['tanggal_lahir'];	//membuat variabel $tanggal_lahir dan datanya dari inputan tanggal lahir
+	$no_hp			= $_POST['no_hp'];		//membuat variabel $no_hp dan datanya dari inputan no hp
+	$jam			= $_POST['jam'];		//membuat variabel $jam dan datanya dari inputan jam
+	$menit			= $_POST['menit'];		//membuat variabel $menit dan datanya dari inputan menit
 	
 	//melakukan query dengan perintah INSERT INTO untuk memasukkan data ke database
 	$input = mysqli_query($mysqli, "INSERT INTO karyawan VALUES('$nik', '$nama', '$tanggal_lahir', '$no_hp', '$jam', $menit)") or die(mysql_error());
