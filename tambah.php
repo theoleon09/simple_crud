f78f9252e60bf55930fd8a87f42a4a8afa1125d2
Vincent Theonica <vincent085213@gmail.com>
2021-09-09 19:34:09 +0700

    mengedit judul

--- tambah.php
+++ tambah.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Simple CRUD by TUTORIALWEB.NET</title>
+	<title>Simple CRUD</title>
 </head>
 <body>
 	<h2>Simple CRUD</h2>
 	
 	<p><a href="index.php">Beranda</a> / <a href="tambah.php">Tambah Data</a></p>
 	
-	<h3>Tambah Data Siswa</h3>
+	<h3>Tambah Data Karyawan</h3>
 	
 	<form action="tambah-proses.php" method="post">
 		<table cellpadding="3" cellspacing="0">
